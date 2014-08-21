a8caabfd09e218921f3e1e909f43d8cafd43f851
Ted Timmons <ted@perljam.net>
2014-08-21 14:52:19 -0700

    add user-agent header

--- update.py
+++ update.py
@@ -4,5 +4,5 @@ import grequests
 
 for x in xrange(0,100000):
   print "%i - %i" % (x*100, (x+1)*100)
-  reqs = (grequests.get("http://newsblur.com/reader/feed/%i" % y) for y in xrange(x*100,(x+1)*100))
+  reqs = (grequests.get("http://newsblur.com/reader/feed/%i" % y, headers={'User-Agent': "@tedder42's newsblur manual updater, https://getsatisfaction.com/newsblur/topics/automated-workaround-for-feeds-not-updating"}) for y in xrange(x*100,(x+1)*100))
   print grequests.map(reqs)
